aaae565bab3e605ffc88fb90fd3ae9aa881fa434
mokasp <kasperbouldin@gmail.com>
2025-03-21 17:21:23 -0500

    (add) copying mapper and reducer (task 6-7) from hadoop user to main user

---
+++ hadoop_project/mapper.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python2.7
+"""
+
+"""
+import sys
+
+
+# keep track of the line numbers so we can take care of duplicates
+line_number = 0
+
+# extract the first line in order to know where to start
+# this is needed because hadoop may split this map job into
+# two runs, so the data may be split and out of order
+first_line = sys.stdin.readline().rstrip()
+first_line = first_line.strip().split(',')
+
+# if this is the first portion of the data, then the first line
+# will contain the column names. in this case the previous id should be 0
+# as the starting id will be 1
+if first_line[0] == 'id':
+    previous_id = 0
+# if the first line is not id, we need to make sure the
+# previous id aligns with the starting id
+else:
+    previous_id = int(first_line[0]) - 1
+
+
+# iterate through each line
+for line in sys.stdin:
+
+    # clean up the line of white spaces and seperate by comma
+    line = line.strip()
+    cleaned_line = line.split(',')
+
+    # extract current id
+    employee_id = cleaned_line[0]
+
+    # if this is not the first line we need to make sure the id can
+    # be converted to an int for further comparing
+    if employee_id != 'id':
+        try:
+            int(employee_id)
+        # if it cannot be converted to an int we will just skip this line
+        except ValueError:
+            continue
+
+    # incrememnt the line counter
+    line_number += 1
+
+    # skipping many duplicates. there are so many
+    if line_number < 2163 or previous_id != int(employee_id) - 1:
+        continue
+
+    # extract the company name
+    company = cleaned_line[1]
+
+    # some of the compensations listed are NaN so we need to set
+    # those to 0 so they can be compared with ints
+    try:
+        compensation = cleaned_line[3]
+    except IndexError:
+        compensation = '0'
+
+    # handling problematic lines where columns are out of order
+    # and removing any apostrophy
+    if employee_id == '5808':
+        company = 'Bank of America Merill Lynch'
+        compensation = '132000'
+    elif employee_id == '55811':
+        company = "Macys"
+        compensation = '150000'
+    elif employee_id == '56931':
+        company = "Macys"
+        compensation = '150000'
+    elif employee_id == '57088':
+        company = "Macys"
+        compensation = '138000'
+    elif employee_id == '57985':
+        company = "Macys"
+        compensation = '115000'
+    elif employee_id == '58927':
+        company = "Macys"
+        compensation = '125000'
+
+    # print the id, company, and salary in the specified format
+    if previous_id == 0:
+        print('id\ttotalyearlysalary,company')
+    print(employee_id + '\t' + company + ',' + compensation)
+
+    # increment the previous id
+    previous_id += 1
---
+++ hadoop_project/reducer.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python2.7
+""
+
+""
+import sys
+import re
+
+# keep track of top salaries
+top_salaries = []
+
+# start line count
+line_number = 0
+
+# go through each each line from the mapper
+for line in sys.stdin:
+
+    # clean the lines of whitespaces
+    line = line.strip()
+
+    # we only need to work with the rows with data, not the labels
+    if not line.startswith("id"):
+        # split the line without storing an array
+        employee_id, company, salary = re.split(r'\t|,', line)
+
+        # first we need to populate the top salaries list until it is full
+        if len(top_salaries) <= 9:
+
+            # append the information about the first few companies to start
+            # the process
+            top_salaries.append((employee_id, company, salary))
+
+            # each time the list is updated it needs to be sorted
+            # to enable easy compaing
+            top_salaries.sort(key=lambda x: int(x[2]), reverse=True)
+
+
+        # once it has 10 elements we can start comparing salaries
+        else:
+
+            # check if the salaru of this current line is greater than the
+            # smallest salary 
+            if int(salary) > int(top_salaries[9][2]):
+                top_salaries[-1] = (employee_id, company, salary)
+                top_salaries.sort(key=lambda x: int(x[2]), reverse=True)
+
+    # incremenmt the line counter
+    line_number += 1
+
+# sort the top salaries list one final time for good measure
+top_salaries.sort(key=lambda x: int(x[2]), reverse=True)
+
+# first print the labels
+print('id\tSalary\tcompany')
+# print each top salary in ascending order
+for j in range(len(top_salaries)):
+    print(top_salaries[j][0] + '\t' + str(float(top_salaries[j][2])) + '\t' + top_salaries[j][1])
