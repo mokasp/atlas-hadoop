a23a63301467a1a3545a3a704ba0c1f8ccf0e2a8
mokasp <kasperbouldin@gmail.com>
2025-03-21 18:02:53 -0500

    (add) partial documentation

--- hadoop_project/5-deletedir.py
+++ hadoop_project/5-deletedir.py
@@ -1,8 +1,40 @@
 #!/usr/bin/env python2.7
+""" This file contains a function that utilizes snakebite's Client object to
+    connect to and interact with a hadoop distributed file system 
+    
+    Functions
+    ---------
+    deletedir(l) : takes in a list of names of directories to delete from
+        the Hadoop Distributed File System
+
+
+    Dependencies
+    ------------
+
+
+
+    Usage
+    -----
+    
+
+    
+    """
 from snakebite.client import Client
 
 
 def deletedir(l):
+    """ function that deletes directories from the Hadoop Distributed File
+        system
+        
+        Args
+        ----
+        l (list) : list of directory names to remove from HDFS
+        
+        Returns
+        -------
+        None
+
+        """
     client = Client('localhost', 9000)
     result = client.delete([l[0]], recurse=True)
     print(list(result))
