b642003578a9d34846e7d0b336bfe62a65ff56fe
mokasp <kasperbouldin@gmail.com>
2025-03-21 17:36:55 -0500

    (remove) testing print statements and comments

--- hadoop_project/4-createdir.py
+++ hadoop_project/4-createdir.py
@@ -4,11 +4,8 @@ from snakebite.client  import Client
 
 def createdir(l):
     client = Client('localhost', 9000)
-    print('hi')
+
 
     for file in l:
         result = client.mkdir([file])
         print(list(result))
-    
-    # result = client.delete([l[0]], recurse=True)
-    # print(list(result))
