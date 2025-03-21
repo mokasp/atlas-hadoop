a735cc31e8b5a7fc5d07aeb761effe44aa15d00a
mokasp <kasperbouldin@gmail.com>
2025-03-21 17:42:23 -0500

    (fix) pycodestyle

--- hadoop_project/4-createdir.py
+++ hadoop_project/4-createdir.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python2.7
-from snakebite.client  import Client
+from snakebite.client import Client
 
 
 def createdir(l):
     client = Client('localhost', 9000)
-
 
     for file in l:
         result = client.mkdir([file])
--- hadoop_project/6-download.py
+++ hadoop_project/6-download.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python2.7
-from snakebite.client  import Client
+from snakebite.client import Client
 
 
 def download(l):
