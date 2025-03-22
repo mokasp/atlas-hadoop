35271d821c6a6e2709ec6fbddfec2737a8a91faf
mokasp <kasperbouldin@gmail.com>
2025-03-21 22:06:49 -0500

    (fix) documentation spacing

--- hadoop_project/4-createdir.py
+++ hadoop_project/4-createdir.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python2.7
-""" This file contains a function that utilizes snakebite's Client object to
+"""
+    This file contains a function that utilizes snakebite's Client object to
     connect to and interact with a hadoop distributed file system 
     
     Functions
@@ -23,13 +24,13 @@
     to check if it was properly created:
         hadoop fs -ls R /
 
-    
-    """
+"""
 from snakebite.client import Client
 
 
 def createdir(l):
-    """ function that creates directories on the Hadoop Distributed File
+    """
+        function that creates directories on the Hadoop Distributed File
         system
         
         Args
@@ -40,7 +41,7 @@ def createdir(l):
         -------
         None
         
-        """
+    """
     client = Client('localhost', 9000)
 
     for file in l:
